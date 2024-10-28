57372f7b6e3553040bbcde8d9d0dd478f6791e7b
Shae Kuhnke <sjkuhnke@uwm.edu>
2024-10-28 15:03:29 -0500

    Configured Google reCAPTCHA to validate contact us forms!

--- rkvApp/views.py
+++ rkvApp/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.conf import settings
@@ -25,14 +26,34 @@ def projects(request):
 
 
 def contact(request):
+    recaptcha_site_key = settings.GOOGLE_RECAPTCHA_SITE_KEY
+
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         subject = request.POST.get('subject')
         message = request.POST.get('message')
+        recaptcha_response = request.POST.get('g-recaptcha-response')
 
         if not name or not email or not subject or not message:
-            return render(request, 'contact.html', {'error': 'All fields are required.'})
+            return render(request, 'contact.html', {
+                'error': 'All fields are required.',
+                'recaptcha_site_key': recaptcha_site_key
+            })
+
+        recaptcha_verify_url = 'https://www.google.com/recaptcha/api/siteverify'
+        recaptcha_data = {
+            'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+            'response': recaptcha_response
+        }
+        recaptcha_result = requests.post(recaptcha_verify_url, data=recaptcha_data)
+        recaptcha_result_json = recaptcha_result.json()
+
+        if not recaptcha_result_json['success']:
+            return render(request, 'contact.html', {
+                'error': 'reCAPTCHA validation failed. Please try again.',
+                'recaptcha_site_key': recaptcha_site_key
+            })
 
         email_subject = 'New Contact Submission: ' + subject
         email_body = render_to_string('contact_email.txt', {
@@ -58,12 +79,19 @@ def contact(request):
         try:
             email_message1.send()
             email_message2.send()
-            return render(request, 'contact.html',
-                          {'success': 'Thank you for your message. We will get back to you shortly.'})
+            return render(request, 'contact.html', {
+                'success': 'Thank you for your message. We will get back to you shortly.',
+                'recaptcha_site_key': recaptcha_site_key
+            })
         except Exception as e:
-            return render(request, 'contact.html', {'error': f'An error occurred: {str(e)}'})
+            return render(request, 'contact.html', {
+                'error': f'An error occurred: {str(e)}',
+                'recaptcha_site_key': recaptcha_site_key
+            })
 
-    return render(request, 'contact.html')
+    return render(request, 'contact.html', {
+        'recaptcha_site_key': recaptcha_site_key
+    })
 
 
 def custom_404(request, exception):
